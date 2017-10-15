4c2af4199029abd20f99fc680845177f2788b61e
JuliaSese <julia.sese0@gmail.com>
2017-10-15 19:53:26 +0200

    corrections

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
-<?php require 'inc/head.php'; ?>
+
+<?php require 'inc/head.php';
+session_start ();
+
+header ('location: index.php');
+
+ ?>
+
 <div class="container" style="margin-top:40px">
 <div class="row">
   <div class="col-sm-6 col-md-4 col-md-offset-4">
